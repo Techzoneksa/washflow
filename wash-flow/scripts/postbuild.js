10f4503553bb74614164fa0fae76d71a68b3597f
Techzoneksa <techzoneksa@gmail.com>
2026-05-25 11:18:55 +0300

    Add static export config for Hostinger deployment; postbuild moves out/ to .next/

---
+++ wash-flow/scripts/postbuild.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const root = path.resolve(__dirname, '..');
+const outDir = path.join(root, 'out');
+const nextDir = path.join(root, '.next');
+
+if (fs.existsSync(outDir)) {
+  if (fs.existsSync(nextDir)) {
+    fs.rmSync(nextDir, { recursive: true, force: true });
+  }
+  fs.renameSync(outDir, nextDir);
+  console.log('Moved out/ -> .next/');
+} else {
+  console.log('No out/ directory found, skipping move');
+}
